90a79687dcd9feafc64d12f607a71c163e604e5b
majidghol <giba.gibson87@gmail.com>
2026-06-08 19:22:17 +0330

    first commit

---
+++ app/src/main/java/com/creatortoolkit/pro/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.creatortoolkit.pro
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        title = "Creator Toolkit Pro"
+    }
+}
