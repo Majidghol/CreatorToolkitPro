71746756db8c347923d46b23cb170eed0cd06b8d
majidghol <giba.gibson87@gmail.com>
2026-06-08 19:47:11 +0330

    Fix white screen

--- app/src/main/java/com/creatortoolkit/pro/MainActivity.kt
+++ app/src/main/java/com/creatortoolkit/pro/MainActivity.kt
@@ -7,7 +7,6 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        title = "Creator Toolkit Pro"
+        setContentView(R.layout.activity_main)
     }
 }
